d43d610c8083c4f8ae6b6315c38818c5bff218e4
mariosa2002 <mariosa2002@hotmail.com>
2023-05-22 14:32:37 +0200

    Actualización 1

--- app/src/main/java/com/example/tfg4/Maps.java
+++ app/src/main/java/com/example/tfg4/Maps.java
@@ -2,14 +2,22 @@ package com.example.tfg4;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.Manifest;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,6 +35,8 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    private LocationManager locationManager;
+
     public static ArrayList<SportsCenter> global = new ArrayList<SportsCenter>();
 
     @SuppressLint("MissingInflatedId")
@@ -34,6 +44,20 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, 1);
+            return;
+        }
+
+       // locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
         mapFragment.getMapAsync(this);
@@ -68,6 +92,12 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
             }
         });
     }
+
+   /* @Override
+    public void onLocationChanged(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+    } */
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
